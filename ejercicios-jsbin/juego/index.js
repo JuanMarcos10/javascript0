b235a9efbffb33ad9177c2a2483f5fdafd22a53b
Juan Marcos <juanmarcosmarinperez@gmail.com>
2020-03-24 19:25:50 +0100

    Avanzando con JSd0 finalizado cap6

---
+++ ejercicios-jsbin/juego/index.js
@@ -0,0 +1,27 @@
+let plan = [
+"                                                                                ",
+"                                                                                ",
+"                                                                                ",
+"                                                                                ",
+"                                                                                ",
+"                                                                                ",
+"                                                                xxx             ",
+"                                                     xx    xx  xx!xx            ",
+"                                        o   o  xx              x!!!x            ",
+"                                                                x!x             ",
+"                                        xxxxx                   xvx             ",
+"                                                                             xx ",
+"  xx                                          o  o                            x ",
+"  x                      o                                                    x ",
+"  x                                          xxxxxx                         o x ",
+"  x               xxxx   o                                                    x ",
+"  x  @            x  x                                         xxxx           x ",
+"  xxxxxxxxxxxxxxxxx  xxxxxx  xxxxxxxx   xxxxxxxxxxxxxxx     xxxx  xxxxxxxxxxxxx ",
+"                                    x   x             x     x                   ",
+"                                    x!!!x             x!!!!!x                   ",
+"                                    x!!!x             x!!!!!x                   ",
+"                                    xxxxx             xxxxxxx                   ",
+"                                                                                "
+];
+let level = new Level(plan);
+let display = new DOMDisplay(document.body, level);
